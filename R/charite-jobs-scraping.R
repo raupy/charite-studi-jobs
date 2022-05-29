5182441a47d9aa42af5126c45461d2109d0ec503
raupy <lilli.schmidt@stu.eh-berlin.de>
2022-05-29 10:30:48 +0200

    refactor: Make code more readable

--- R/charite-jobs-scraping.R
+++ R/charite-jobs-scraping.R
@@ -8,20 +8,71 @@ library(lubridate)
 library(assertive)
 library(rebus)
 
+# **********************
+# Preparation ----
+# **********************
+
+## ├ Throttling GET function (gentle webscraping) ----
+
 slow_GET <- slowly(~ GET(.), rate = rate_delay(1), quiet = TRUE)
+
+
+## ├ Build a vector of urls to crawl ----
+
+get_sites_to_crawl <- function(url) {
+  resp <- slow_GET(url)
+  assert_are_set_equal(resp$status_code, 200)
+
+  number_of_results_text <- content(resp) %>%
+    html_node(".tx-indexedsearch-browsebox") %>%
+    html_nodes("p") %>%
+    html_text()
+
+  number_of_results_vec <- str_extract_all(
+    number_of_results_text,
+    "\\(?[0-9,.]+\\)?")[[1]] %>%
+    as.numeric()
+  if (length(number_of_results_vec) == 0) {
+    warning("The length of number_of_results_vec is 0.
+            Cannot find out the number of sites to crawl.")
+    sites <- url
+  } else {
+    assert_are_set_equal(length(number_of_results_vec), 3)
+    assert_is_not_null(number_of_results_vec[2])
+
+    number_of_pages <- as.integer(number_of_results_vec[3] /
+                                    number_of_results_vec[2])
+    sites <- c(url,
+               paste0(url, "seite/") %>%
+                 paste0(1:number_of_pages)
+               )
+  }
+  sites
+}
+
+# **********************
+# Scraping functions ----
+# **********************
 
 map_p_tags <- function(links, date_regex) {
     p_tags_text <- links %>%
       html_nodes("p") %>%
       html_text()
+
     date_indices <- p_tags_text %>%
       str_detect(date_regex) %>%
       which()
+
     if (length(date_indices) == 0) {
       res <- "no dates"
     } else {
-      split_list <- split(date_indices, ceiling(seq_along(date_indices) / 2))
-      res <- map(map(split_list, ~ .x[1]:.x[2]), ~ p_tags_text [.x])
+      split_list <- split(
+        date_indices,
+        ceiling(seq_along(date_indices) / 2)
+        )
+      res <- map(map(split_list, ~ .x[1]:.x[2]),
+                 ~ p_tags_text[.x]
+                 )
     }
     res
 }
@@ -31,7 +82,10 @@ extract_features <- function(mapped_p_tags, date_regex) {
     map(~ str_remove(.x, "Zum Stellenangebot")) %>%
     map(~ str_remove(.x, "Bewerbungsfrist:")) %>%
     map(~ str_trim(.x))
-  dates <- map(cleaned_text, ~ str_extract_all(.x, date_regex)) %>%
+
+  dates <- map(cleaned_text,
+               ~ str_extract_all(.x, date_regex)
+               ) %>%
     unlist() %>%
     dmy()
   odds <- seq(1, length(dates), 2)
@@ -41,30 +95,38 @@ extract_features <- function(mapped_p_tags, date_regex) {
     map_chr(~ paste0(.x, collapse = ": "))
 
   tibble(institute = cleaned_text,
-                           published = dates[odds],
-                           deadline = dates[-odds]) %>%
-    mutate(institute = ifelse(str_detect(institute, "[a-z]"),
-                             str_remove_all(institute, "-") %>%
-                               map_chr(~ str_remove(.x, ":")) %>%
-                               map_chr(~ str_sub(.x, end = -3)) %>%
-                               map_chr(~ str_trim(.x)),
-                             "-"))
+         published = dates[odds],
+         deadline = dates[-odds]) %>%
+    mutate(institute = ifelse(
+      str_detect(institute, "[a-z]"),
+      str_remove_all(institute, "-") %>%
+        map_chr(~ str_remove(.x, ":")) %>%
+        map_chr(~ str_sub(.x, end = -3)) %>%
+        map_chr(~ str_trim(.x)),
+      "-"))
 }
 
 find_list_jobs <- function(url, search_string) {
   resp <- slow_GET(url)
   assert_are_set_equal(resp$status_code, 200)
+
   html <- content(resp)
-  links <- html %>% html_nodes(".list-jobs") %>% html_nodes("li a")
+  links <- html %>%
+    html_nodes(".list-jobs") %>%
+    html_nodes("li a")
   html_text <- links %>% html_text()
-  search_result <- which(str_detect(html_text, search_string))
+
+  search_result <- str_detect(html_text, search_string)
   jobs_tbl <- tibble()
-  if (length(search_result) > 0) {
+
+  if (sum(search_result) > 0) {
     results <- links[search_result]
-    date_regex <- "(0[1-9]|[12][0-9]|3[01]).(0[1-9]|1[0-2]).\\d{4}"
     urls <- results %>% html_attr("href")
     urls <- paste0("https://www.charite.de", urls)
+
+    date_regex <- "(0[1-9]|[12][0-9]|3[01]).(0[1-9]|1[0-2]).\\d{4}"
     mapped_p_tags <- map_p_tags(results, date_regex)
+
     if (class(mapped_p_tags) == "character") {
       jobs_tbl <- tibble(institute = html_text,
                          published = dmy(NA),
@@ -79,68 +141,66 @@ find_list_jobs <- function(url, search_string) {
   jobs_tbl
 }
 
-get_sites_to_crawl <- function(url) {
+
+# **********************
+# Job details ----
+# **********************
+
+
+get_job_details <- function(url, search_string = "") {
   resp <- slow_GET(url)
   assert_are_set_equal(resp$status_code, 200)
-  number_of_results_text <- content(resp) %>%
-    html_node(".tx-indexedsearch-browsebox") %>%
-    html_nodes("p") %>%
+  job <- content(resp)
+
+  job_facts <- job %>%
+    html_nodes("article h2#headline2 ~ p") %>%
     html_text()
-  number_of_results_vec <- str_extract_all(
-    number_of_results_text,
-    "\\(?[0-9,.]+\\)?")[[1]] %>%
-    as.numeric()
-  if (length(number_of_results_vec) == 0) {
-    warning("The length of number_of_results_vec is 0.
-            Cannot find out the number of sites to crawl.")
-    sites <- url
-  } else {
-    assert_are_set_equal(length(number_of_results_vec), 3)
-    assert_is_not_null(number_of_results_vec[2])
-    number_of_pages <- as.integer(number_of_results_vec[3] /
-                                    number_of_results_vec[2])
-    sites <- c(url, paste0(url, "seite/") %>%
-                 paste0(1:number_of_pages))
-  }
-  sites
+
+  job_description <- job %>%
+    html_nodes("article li") %>%
+    html_text()
+
+  job_details <- tibble(
+    starting_date = job_facts[1],
+    limited_for = str_extract(
+      job_facts[2],
+      pattern = or(one_or_more(DGT) %R%
+                     SPACE %R%
+                     one_or_more(WRD),
+                   DMY)),
+    hours_per_month = str_extract(
+      job_facts[3],
+      pattern = one_or_more(DGT)
+      ) %>%
+      as.numeric(),
+    contains_search_string = str_detect(
+      job_description,
+      pattern = or(START, one_or_more(SPACE)) %R%
+        search_string %R%
+        or(one_or_more(SPACE), DOT, ",")
+      ) %>%
+      sum() > 0
+    )
+  job_details
 }
+
+
+# **********************
+# Map_x function wrappers ----
+# **********************
 
 get_all_jobs <- function(urls, search_string = "") {
   map_dfr(urls, find_list_jobs, search_string)
 }
 
-
-get_job_details <- function(url, search_string = ""){
-  resp <- slow_GET(url)
-  assert_are_set_equal(resp$status_code, 200)
-  job <- content(resp)
-  job_facts <- job %>%
-    html_nodes("article h2#headline2 ~ p") %>%
-    html_text()
-  job_description <- job %>%
-    html_nodes("article li") %>%
-    html_text()
-  job_details <- tibble(starting_date = job_facts[1],
-                     limited_for = str_extract(job_facts[2],
-                                               pattern =
-                                                 or(one_or_more(DGT) %R%
-                                                      SPACE %R%
-                                                      one_or_more(WRD),
-                                                    DMY)),
-                     hours_per_month = str_extract(job_facts[3],
-                                                   pattern = one_or_more(DGT)) %>% as.numeric(),
-                     contains_search_string = str_detect(job_description,
-                                                         pattern =
-                                                           or(START, one_or_more(SPACE)) %R%
-                                                           search_string %R%
-                                                           or(one_or_more(SPACE), DOT, ",")) %>%
-                       sum() > 0)
-  job_details
-}
-
 get_all_job_details <- function(urls, search_string = "") {
   map_dfr(urls, get_job_details, search_string)
 }
+
+# **********************
+# Function calls ----
+# **********************
+
 
 url <- "https://www.charite.de/karriere/stellenboerse/"
 search_string <- "STUD"
